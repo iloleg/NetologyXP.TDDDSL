56a86a9eeea39e89e3258c5e77110826b068e21b
Oleg <il.oleg@outlook.com>
2017-02-27 17:10:40 +0200

    create test  customer ordered a salad with chicken, cucumbers,onion,OliveOil,Salt,Pepper

--- tests/when-ordering-salad-dsl.js
+++ tests/when-ordering-salad-dsl.js
@@ -6,8 +6,8 @@
 var assert = require('chai').assert;
 var SaladBuilder = require('../tests/dsl/salad-builder');
 
-suite('when ordering salad', function () {
-    test('customer ordered a salad with chicken', function () {
+suite('when ordering salad witch chicken', function () {
+    test('customer ordered a salad all include', function () {
 
         let salad = new SaladBuilder()
             .withName("Witch chicken")
@@ -17,6 +17,22 @@ suite('when ordering salad', function () {
             .withOnion()
             .withOliveOil()
             .withVegetableOil()
+            .withSalt()
+            .withPepper()
+            .please();
+
+
+
+        assert.equal(300);
+    });
+    test('customer ordered a salad with chicken, cucumbers,onion,OliveOil,Salt,Pepper ', function () {
+
+        let salad = new SaladBuilder()
+            .withName("Witch Chicken witch Cucumbers and Onion and OliveOil and Salt and Pepper")
+            .withChicken()
+            .withCucumbers()
+            .withOnion()
+            .withOliveOil()
             .withSalt()
             .withPepper()
             .please();
