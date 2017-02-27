b883f2db36aa8afabf5a58b3db5c342ab61ae013
Oleg <il.oleg@outlook.com>
2017-02-27 17:14:57 +0200

    create test

--- tests/when-ordering-salad-dsl.js
+++ tests/when-ordering-salad-dsl.js
@@ -41,4 +41,73 @@ suite('when ordering salad witch chicken', function () {
 
         assert.equal(300);
     });
+
+    test('customer ordered a salad with chicken, cucumbers, tomato,onion,VegetableOil,Salt,Pepper ', function () {
+
+        let salad = new SaladBuilder()
+            .withName("Witch Chicken witch Cucumbers and Tomato and Onion and VegetableOil and Salt and Pepper")
+            .withChicken()
+            .withCucumbers()
+            .withTomato()
+            .withOnion()
+            .withVegetableOil()
+            .withSalt()
+            .withPepper()
+            .please();
+
+
+
+        assert.equal(300);
+    });
+});
+suite('when ordering salad', function () {
+    test('customer ordered a salad all include', function () {
+
+        let salad = new SaladBuilder()
+            .withName("Salad")
+            .withCucumbers()
+            .withTomato()
+            .withOnion()
+            .withOliveOil()
+            .withVegetableOil()
+            .withSalt()
+            .withPepper()
+            .please();
+
+
+
+        assert.equal(300);
+    });
+    test('customer ordered a salad with cucumbers,onion,OliveOil,Salt,Pepper ', function () {
+
+        let salad = new SaladBuilder()
+            .withName("Witch  Cucumbers and Onion and OliveOil and Salt and Pepper")
+            .withCucumbers()
+            .withOnion()
+            .withOliveOil()
+            .withSalt()
+            .withPepper()
+            .please();
+
+
+
+        assert.equal(300);
+    });
+
+    test('customer ordered a salad with cucumbers, tomato,onion,VegetableOil,Salt,Pepper ', function () {
+
+        let salad = new SaladBuilder()
+            .withName("Witch Cucumbers and Tomato and Onion and VegetableOil and Salt and Pepper")
+            .withCucumbers()
+            .withTomato()
+            .withOnion()
+            .withVegetableOil()
+            .withSalt()
+            .withPepper()
+            .please();
+
+
+
+        assert.equal(300);
+    });
 });
